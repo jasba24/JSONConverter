36fccd71a5c28089cb8a3c7842ab5d205eff7779
Sebastian Buitrago <johanbuitrago342@gmail.com>
2021-12-02 07:47:15 -0500

    edit converter.go

--- converter.go
+++ converter.go
@@ -6,9 +6,9 @@ import (
 )
 
 func Converter(class interface{}, path string) interface{} {
-	file, _ := ioutil.ReadFile(path)
+	file, err := ioutil.ReadFile(path)
 	file_struct := class
 	json.Unmarshal(file, &file_struct)
 
-	return file_struct
+	return file_struct, err
 }
